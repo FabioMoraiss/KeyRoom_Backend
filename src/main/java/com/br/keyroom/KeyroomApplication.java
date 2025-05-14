124326a6c0e297c4c2c98c49f2800da8f05bd70b
fabiomoraiss <rarinaldo@outlook.com>
2025-05-13 21:42:04 -0300

    spring boot initializer

---
+++ src/main/java/com/br/keyroom/KeyroomApplication.java
@@ -0,0 +1,13 @@
+package com.br.keyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/keyroom/KeyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.keyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
