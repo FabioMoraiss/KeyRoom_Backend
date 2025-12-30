8b8ea18dae93059f55ead664a0bc7f2fc2432d9b
fabiomoraiss <rarinaldo@outlook.com>
2025-12-30 15:38:15 -0300

    first commit, criando o projeto no spring initializer

---
+++ src/main/java/keyroom/com/br/keyroomapi/KeyroomapiApplication.java
@@ -0,0 +1,13 @@
+package keyroom.com.br.keyroomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyroomapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyroomapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keyroom/com/br/keyroomapi/KeyroomapiApplicationTests.java
@@ -0,0 +1,13 @@
+package keyroom.com.br.keyroomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyroomapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
